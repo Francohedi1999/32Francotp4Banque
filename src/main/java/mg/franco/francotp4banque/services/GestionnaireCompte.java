e782e4a7cb0cc926b1c4ce24c34d2987ff1871b2
Francohedi1999 <ramarotafika1999@gmail.com>
2024-04-06 09:40:31 +0300

    TP 4: Déclaration de la source de données

---
+++ src/main/java/mg/franco/francotp4banque/services/GestionnaireCompte.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.franco.francotp4banque.services;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Franco
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",
+    password="0000", 
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
+
+@RequestScoped
+public class GestionnaireCompte 
+{
+    
+}
